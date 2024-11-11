68d7af053e96de4888275813f69762946477549d
Marwan Alrifai <marwanalrfai5@gmail.com>
2024-11-12 00:02:34 +0300

    fixed some problem on pipex

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -17,13 +17,13 @@ void	exec(char *cmd, char **env)
 	char	**s_cmd;
 	char	*path;
 
-	s_cmd = ft_split(cmd, ' ');
-	path = get_path(s_cmd[0], env);
+	s_cmd = ft_split(cmd, ' ');//
+	path = get_path(s_cmd[0], env);//
 	if (execve(path, s_cmd, env) == -1)
 	{
 		ft_putstr_fd("pipex: command not found: ", 2);
 		ft_putendl_fd(s_cmd[0], 2);
-		free_tab(s_cmd);
+		free_tab(s_cmd);//
 		exit(0);
 	}
 }
@@ -32,22 +32,27 @@ void	child(char **av, int *p_fd, char **env)
 {
 	int		fd;
 
-	fd = open_file(av[1], 0);
-	dup2(fd, 0);
-	dup2(p_fd[1], 1);
+	fd = open_file(av[1], 0);//
+	dup2(fd, 0);//
+	dup2(p_fd[1], 1);//
 	close(p_fd[0]);
+	close(fd);
 	exec(av[2], env);
 }
 
 void	parent(char **av, int *p_fd, char **env)
 {
 	int		fd;
+	pid_t	pid;
 
 	fd = open_file(av[4], 1);
-	dup2(fd, 1);
-	dup2(p_fd[0], 0);
+	dup2(fd, 1);//
+	dup2(p_fd[0], 0);//
 	close(p_fd[1]);
-	exec(av[3], env);
+	close(fd);
+	pid = fork();//
+	if (pid == 0)
+		exec(av[3], env);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -59,10 +64,20 @@ int	main(int argc, char **argv, char **env)
 		exit_handler(1);
 	if (pipe(p_fd) == -1)
 		exit(-1);
-	pid = fork();
+	pid = fork();//
 	if (pid == -1)
 		exit(-1);
 	if (!pid)
 		child(argv, p_fd, env);
 	parent(argv, p_fd, env);
+	close(p_fd[0]);
+	close(p_fd[1]);
+	return 0;
 }
+
+
+
+
+
+
+
